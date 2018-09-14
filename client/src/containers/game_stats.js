016f86b62c9a3c1d5cad4f1a1798f1ef474900e0
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-09-14 14:18:39 -0400

    added chart data for each game mode and wrote render mode chart function

--- client/src/containers/game_stats.js
+++ client/src/containers/game_stats.js
@@ -8,6 +8,121 @@ componentDidMount () {
   this.renderCharts()
 }
 
+createModeCharts () {
+  const soloGames = this.props.games.filter(game =>
+    { return (game.mode === 'Solo') })
+
+    const soloKills = [];
+     const SoloKills = soloGames.map(game => {
+       return soloKills.push(game.max_kills)
+      }
+    );
+
+    const soloFinalPlace = [];
+      const SoloFinalPlace = soloGames.map(game => {
+        return soloFinalPlace.push(game.final_place)
+        });
+
+  const squadGames = this.props.games.filter(game =>
+    { return (game.mode === 'Squads') })
+
+    const squadKills = [];
+    const SquadKills = squadGames.map(game => {
+      return squadKills.push(game.max_kills)
+     }
+   );
+
+   const squadFinalPlace = [];
+     const SquadFinalPlace = squadGames.map(game => {
+       return squadFinalPlace.push(game.final_place)
+       });
+
+  const fiftyvGames = this.props.games.filter(game =>
+    { return (game.mode === '50v50') })
+
+  const fiftyvKills = [];
+  const FiftyvKills = fiftyvGames.map(game => {
+    return fiftyvKills.push(game.max_kills)
+   }
+ );
+
+ const fiftyvFinalPlace = [];
+   const FiftyvFinalPlace = fiftyvGames.map(game => {
+     return fiftyvFinalPlace.push(game.final_place)
+     });
+
+  const duoGames = this.props.games.filter(game =>
+    { return (game.mode === 'Duos') })
+
+    const duoKills = [];
+    const DuoKills = duoGames.map(game => {
+      return duoKills.push(game.max_kills)
+     }
+   );
+
+// calc final place over time while playing duos
+   const duoFinalPlace = [];
+     const DuoFinalPlace = duoGames.map(game => {
+       return duoFinalPlace.push(game.final_place)
+       });
+
+  const playgroundGames = this.props.games.filter(game =>
+    { return (game.mode === 'Playground') })
+
+  const playgroundKills = [];
+  const PlaygroundKills = playgroundGames.map(game => {
+    return playgroundKills.push(game.max_kills)
+   }
+ );
+
+//  to calc data for final position playing playground mode
+ const pgFinalPlace = [];
+   const PgFinalPlace = playgroundGames.map(game => {
+     return pgFinalPlace.push(game.final_place)
+     });
+
+ return (
+   <tbody>
+       <tr>
+       <td> Total Kills </td>
+         <td>
+           <Chart  data={soloKills} color="red" svgWidth={25} svgHeight={20} />
+         </td>
+         <td>
+           <Chart  data={duoKills} color="purple" svgWidth={25} svgHeight={20} />
+         </td>
+         <td>
+           <Chart  data={squadKills} color="orange" svgWidth={25} svgHeight={20} />
+         </td>
+         <td>
+           <Chart  data={fiftyvKills} color="green" svgWidth={25} svgHeight={20} />
+         </td>
+         <td>
+           <Chart  data={playgroundKills} color="blue" svgWidth={25} svgHeight={20} />
+         </td>
+       </tr>
+       <tr>
+       <td> Final Position </td>
+         <td>
+           <Chart  data={soloFinalPlace} color="red" svgWidth={25} svgHeight={20} />
+         </td>
+         <td>
+           <Chart  data={duoFinalPlace} color="purple" svgWidth={25} svgHeight={20} />
+         </td>
+         <td>
+           <Chart  data={squadFinalPlace} color="orange" svgWidth={25} svgHeight={20} />
+         </td>
+         <td>
+           <Chart  data={fiftyvFinalPlace} color="green" svgWidth={25} svgHeight={20} />
+         </td>
+         <td>
+           <Chart  data={pgFinalPlace} color="blue" svgWidth={25} svgHeight={20} />
+         </td>
+       </tr>
+    </tbody>
+ )
+
+}
 
  renderCharts(){
    // debugger;
@@ -16,34 +131,6 @@ componentDidMount () {
    } else {
      console.log("This.props:", this.props.games);
 
-     // const solo = []
-    const soloGames = this.props.games.filter(game =>
-      {
-      return (game.mode === 'Solo')
-    })
-
-    const squadGames = this.props.games.filter(game =>
-      {
-      return (game.mode === 'Squads')
-    })
-
-    const fiftyvGames = this.props.games.filter(game =>
-      {
-      return (game.mode === '50v50')
-    })
-
-    const duosGames = this.props.games.filter(game =>
-      {
-      return (game.mode === 'Duos')
-    })
-
-    const playgroundGames = this.props.games.filter(game =>
-      {
-      return (game.mode === 'Playground')
-    })
-
-
-    debugger;
   const overallKills = [];
    const OverallKills = this.props.games.map(game => {
      return overallKills.push(game.max_kills)
@@ -77,26 +164,31 @@ componentDidMount () {
 
   render () {
     return (
-      <table>
-        <thead>
-          <th> Total Kills Overall</th>
-          <th> Final Place Over Time </th>
-        </thead>
-          {this.renderCharts()}
-      </table>
-      // <table className="table table-hover">
-      //   <thead>
-      //     <tr>
-      //       <th> Game Mode</th>
-      //       <th> Total Kills </th>
-      //       <th> Final Position</th>
-      //       <th> Comments</th>
-      //     </tr>
-      //   </thead>
-      //   <tbody>
-      //   {this.renderCharts()}
-      //   </tbody>
-      // </table>
+      <div>
+          <table>
+            <thead>
+              <th> Game Kills Over Time</th>
+              <th> Final Place Over Time </th>
+            </thead>
+              {this.renderCharts()}
+          </table>
+
+       <table className="table table-hover">
+         <thead>
+           <tr>
+            <th> Event </th>
+             <th> Solo</th>
+             <th> Duo </th>
+             <th> Squad</th>
+             <th> 50v50 </th>
+                <th> Playground </th>
+           </tr>
+         </thead>
+
+         {this.createModeCharts()}
+
+       </table>
+      </div>
     );
   }
 
