25b8d68e2d81115a379e0ad215472e42342cf1d0
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-09-17 13:55:25 -0400

    removed sparklines from chart.js file

--- client/src/components/chart.js
+++ client/src/components/chart.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 import { Line } from 'react-chartjs-2';
 
 
@@ -30,18 +29,7 @@ const Chart = (props) => {
       }
     ]
   }
-    // debugger;
-
   return(
-    // <div>
-      // <Sparklines height={120} width={180}
-      //   data={props.data}>
-      //   <SparklinesLine  color={props.color} />
-      //   {/*<SparklinesReferenceLine type="avg" />*/}
-      //
-      // </Sparklines>
-
-    // </div>
 
     <div className="chart">
       <Line
