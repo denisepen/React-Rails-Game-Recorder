d66c478bfb56dfd6ae7029837d953a4a5216bd0c
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-09-18 01:08:45 -0400

    added submit functions to newuser form

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NewUser from './new_user'
 
 export default () => {
   return (
@@ -7,6 +8,7 @@ export default () => {
         <p>
         This application allows you to record the data for each of your Fortnite games.  This data will then be collected so the you can track your progress. After adding your games, please check out the stats tab to see your progress over time.
         </p>
+        <NewUser />
     </div>
   )
 }
---
+++ client/src/components/new_user.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+class NewUser extends React.Component {
+  constructor () {
+    super ()
+
+    this.state={
+      name: '',
+      gamer_tag: '',
+      email: '',
+      password: '',
+
+    }
+  }
+
+  handleNameChange = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  handleTagChange = (e) => {
+    this.setState({
+      gamer_tag: e.target.value
+    })
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div>
+
+    <form onSubmit={(event) => this.handleSubmit(event)} className="input">
+      <h2> Please Sign Up </h2>
+      Name: <input type="text" value={this.state.name} onChange={(e) => this.handleNameChange(e)}/>{this.state.name}<br/>
+
+
+      Gamer Tag: <input type="text" value={this.state.gamer_tag} onChange={(e) => this.handleTagChange(e)}/>{this.state.gamer_tag}<br/>
+
+      Email:<input value={this.state.email} type="text" onChange={(e) => this.handleEmailChange(e)}/>{this.state.email}<br/>
+
+      Password: <input value={this.state.password} type="text" onChange={(e) => this.handlePasswordChange(e)}/>{this.state.password}<br/>
+
+
+      <input type="submit" value="Create Account" />
+    </form><br/>
+    </div>
+
+    )
+  }
+}
+
+
+export default NewUser;
