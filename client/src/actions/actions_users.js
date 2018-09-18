568e2665ddae32dfb5ea42e98c700219c44e47fd
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-09-18 01:40:26 -0400

    created adduseraction to action users file

---
+++ client/src/actions/actions_users.js
@@ -0,0 +1,21 @@
+
+
+export function addUser(user) {
+
+      const request = {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+
+      return (dispatch) => {
+        console.log("Dispatched user", user);
+        return fetch('/games', request )
+                 .then(response => response.json())
+                 // .then(response => console.log("api response",response))
+                 //  .then(res => console.log("Game just added to db:", res))
+                 .then(newUser => dispatch({ type: 'ADD_GAME', newUser}))
+      }
+    }
