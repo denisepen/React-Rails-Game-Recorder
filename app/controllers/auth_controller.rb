62b7b289dbcaeae088f1d37a0099738b55ef7016
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-09-20 17:31:21 -0400

    now users create doesn't work

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,8 +1,10 @@
 class AuthController < ApplicationController
+
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       render json: user
+    end
   end
 
 end
